30e041249d408786065798282f5c894aade02291
Taifinkaba <taifinkabad@gmail.com>
2024-04-16 22:23:42 -0400

    stuck on 12

--- src/debug.js
+++ src/debug.js
@@ -1,20 +1,20 @@
 const myEvery = (arr, callback) => {
   for (const value of arr) {
-    if (callback) return false;
+    if (!callback(value)) return false;
   }
   return true;
 };
 
 const sortUsersBy = (users, sortingFunction) => {
-  return [...users].sort(sortingFunction());
+  return [...users].sort(sortingFunction);
 };
 
 const logEachName = (names) => {
-  return names.forEach(console.log());
+  names.forEach(name => console.log(name));
 };
 
 const logEachUserBio = (users) => {
-  return users.forEach(console.log(users.bio));
+  users.forEach(user => console.log(user.bio));
 };
 
 module.exports = {
